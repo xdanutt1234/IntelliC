889915796e5edd6d30432ecbf75c15352033a213
ALEXANDRU STEFANESCU <alexliviu2004@gmail.com>
2024-05-27 20:32:32 +0300

    restructure

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import "./styles.css";
-import App from "./MainPage.svelte";
-
-
-const app = new App({
-  target: document.getElementById("app"),
-});
-
-export default app;
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Adasdw sadas</p>
